f80fbf7cab23dfb8a4221c95567bad70b1cc1860
Harshit Mishra <harshitmishra2011@gmail.com>
2023-01-20 15:28:55 +0530

    create daily notes and bookmark toggle component

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Sidebar.module.css'
 
 const Sidebar = () => {
+    const [select, setselect] = useState(0)
+
+    function handleselect(e){
+        setselect(e.target.id)
+    }
+
   return (
     <div className={styles.sidebar}>
         <div className={styles.header}> <img src="notepad.png" alt="NotepadImage"/> DAILY NOT TRACKER</div>
-        <div className={styles.option} style={{color: "#806BFF"}}> <img src="list.png" alt="ListImage"/> Daily Notes</div>
-        <div className={styles.option} style={{color: "#FF6B6B"}}> <img src="bookmark.png" alt="BookmarkImage"/> Bookmarks</div>
-      
+        <div onClick={handleselect} id="1" className={styles.option} style={select==="1"?{background: "#bebdbd", color: "#806BFF"}:{color: "#806BFF"}}> <img src="list.png" alt="ListImage"/> Daily Notes</div>
+        <div onClick={handleselect} id="2" className={styles.option} style={select==="2"?{background: "#bebdbd", color: "#FF6B6B"}:{color: "#FF6B6B"}}> <img src="bookmark.png" alt="BookmarkImage"/> Bookmarks</div>
     </div>
   )
 }
